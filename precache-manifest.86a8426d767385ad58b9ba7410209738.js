73a637542da38d3643a6914ad4298bf6b5de9967
Eugene Glova <eugene.glova@gmail.com>
2018-12-20 19:05:59 +0300

    Updates

---
+++ precache-manifest.86a8426d767385ad58b9ba7410209738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "661d9ea799b52c997125",
+    "url": "/bitfinex-api-react-redux/static/js/runtime~main.661d9ea7.js"
+  },
+  {
+    "revision": "636662054430c7db18e0",
+    "url": "/bitfinex-api-react-redux/static/js/main.63666205.chunk.js"
+  },
+  {
+    "revision": "fea9193fc85f6765374a",
+    "url": "/bitfinex-api-react-redux/static/js/1.fea9193f.chunk.js"
+  },
+  {
+    "revision": "636662054430c7db18e0",
+    "url": "/bitfinex-api-react-redux/static/css/main.014ca4d3.chunk.css"
+  },
+  {
+    "revision": "bc26424affd8b1542bf3589ef61c844a",
+    "url": "/bitfinex-api-react-redux/index.html"
+  }
+];
